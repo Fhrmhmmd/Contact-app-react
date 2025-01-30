c599e329ec28f349066c71c1859e0724b814aa88
Fhrmhmmd <149661127+Fhrmhmmd@users.noreply.github.com>
2025-01-30 14:39:52 +0700

    Add routing

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,51 +1,25 @@
 import React from 'react';
-import ContactList from './ContactList';
-import { getData } from '../utils/data';
-import ContactInput from './ContactInput';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation';
+import AddPage from '../page/AddPage';
+import HomePage from '../page/HomePage';
 
-class ContactApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: getData(),
-        }
-
-        this.onDeleteHandler = this.onDeleteHandler.bind(this);
-        this.onAddContactHandler = this.onAddContactHandler.bind(this);
-    }
-    
-    onDeleteHandler(id){
-        const contacts = this.state.contacts.filter(contact => contact.id !== id);
-        this.setState({ contacts });
-    }
-
-    onAddContactHandler({ name,tag }) {
-        this.setState((prevState) => {
-            return {
-                contacts: [
-                    ...prevState.contacts,
-                    {
-                        id: +new Date(),
-                        name,
-                        tag,
-                        imageUrl: '/images/default.jpg',
-                    }
-                ]
-            }
-        });
-    }
-    
-    render(){
-        return (
-            <div className='contact-App'>
-                <h1>Contact App</h1>
-                <h2>Add Contact</h2>
-                <ContactInput addContact={this.onAddContactHandler} />
-                <h1>Contact List</h1>
-                <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-            </div>
-        );
-    }
-}
+function ContactApp() {
+    return (
+      <div className="contact-App">
+        <header className='contact-app__header'>
+          <h1>Aplikasi Kontak</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add" element={<AddPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
+  
 
 export default ContactApp;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import ContactApp from "./components/ContactApp";
+import { BrowserRouter } from "react-router-dom";
+
 
 // Styling
 import './styles/style.css';
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<ContactApp />);
+root.render(
+<BrowserRouter>
+<ContactApp />
+</BrowserRouter>);
